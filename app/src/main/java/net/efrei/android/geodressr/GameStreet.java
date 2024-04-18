d9b5c779443adaa671573a2b526327a999875605
JiveOff <antoine@jiveoff.fr>
2024-04-18 09:53:05 +0200

    feat: add gradient + streetview on GameStreet

--- app/src/main/java/net/efrei/android/geodressr/GameStreet.java
+++ app/src/main/java/net/efrei/android/geodressr/GameStreet.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+
 
 /**
  * Etape 2 du jeu : le streetview
@@ -18,5 +21,18 @@ public class GameStreet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_street);
+
+        loadStreetViewFragment();
+    }
+
+
+
+    private void loadStreetViewFragment() {
+        SupportStreetViewPanoramaFragment streetViewPanoramaFragment =
+                (SupportStreetViewPanoramaFragment) getSupportFragmentManager().findFragmentById(R.id.streetViewPanoramaView);
+
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(streetViewPanorama -> {
+            streetViewPanorama.setPosition(new LatLng(48.91522771645218, 2.2295974043058755));
+        });
     }
 }
