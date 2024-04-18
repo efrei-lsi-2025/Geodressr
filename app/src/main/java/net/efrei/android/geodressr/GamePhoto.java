60869a472b29c62665b20241c829dc602a1fbec6
LoganTann <logon313@hotmail.fr>
2024-04-18 09:04:47 +0200

    feat: ajout des activités game

---
+++ app/src/main/java/net/efrei/android/geodressr/GamePhoto.java
@@ -0,0 +1,22 @@
+package net.efrei.android.geodressr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Etape 3 du jeu : prendre en photo le lieu
+ *
+ * Paramètres :
+ * - targetCoords : coordonnées GPS de la cible
+ */
+public class GamePhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_photo);
+    }
+
+
+}
